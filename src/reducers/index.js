4be5f430cc499fe0ea8fc3045cf4f7fd36ce9342
Stephen Handy <a.king.life@pm.me>
2021-08-30 01:45:43 -0400

    initial state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
 
 export const initialState = {
+    loading: false,
+    smurfs: [],
+    error: 'error'
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
 }
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
